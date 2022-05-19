e8ab5dfec17b11c8237682228118a687d68a9665
Nicola Menolascina <nicola.menola1505@gmail.com>
2022-05-19 23:41:30 +0200

    Aggiornamento contenuto.php e modifica home, cancellazione header.html.twig

--- contenuto.php
+++ contenuto.php
@@ -13,45 +13,68 @@ $twig = new Environment($loader, [
     'debug' => true,
   ]);
 $twig->addExtension(new \Twig\Extension\DebugExtension());
-
-$data=array();
-
+$template = $twig->load('home.html.twig');
 $client = new Client();
 
+
+$data=array();
 $api='KbBBIL0bvPv8NVVI3cPZyu3fMIyp8BJE';
-
-$body=array();
-$template = $twig->load('home.html.twig');
-
-if (isset($_POST["invio"])&&isset($_POST["cerca"])){
-    $request = $client->get('https://api.giphy.com/v1/gifs/search?q='.$_POST["cerca"]."&api_key=".$api);
-    if ($request->getBody()) {
-
-      $body = $request->getBody()->getContents();
-      $received_json=json_decode($body, true);
-      $body= $received_json["data"];
-      for ($i=0;$i<20;$i++){
-        array_push($data, [
-          'url' => $body[$i]["images"]["original"]["url"],
-          'title' => $body[$i]["title"],
-          'username' => $body[$i]["username"],
-          'date' => $body[$i]['import_datetime']
-        ]);
-      }
+$cerca = "meme";
+$index= 0;
 
 
+function request($client,$cerca,$api,$index,$data){
+  $request = $client->get('https://api.giphy.com/v1/gifs/search?q='.$cerca."&api_key=".$api."&offset=".$index);
+    
+  if ($request->getBody()) {
+
+    $body = $request->getBody()->getContents();
+    $received_json=json_decode($body, true);
+    $body= $received_json["data"];
+    for ($i=0;$i<20;$i++){
+
+      $date = (string) $body[$i]['import_datetime'];
+      $date = substr($date, 0, -8);
+
+      array_push($data, [
+        'url' => $body[$i]["images"]["original"]["url"],
+        'title' => $body[$i]["title"],
+        'username' => $body[$i]["username"],
+        'date' => $date,
+        'width' => $body[$i]['images']['original']['width'],
+        'height' => $body[$i]['images']['original']['height'],
+        'embed' => $body[$i]['embed_url']
+      ]);
     }
+  }
+  return $data;
 }
 
 
-echo $template->render([
-  'dati' => $data,
-  'menu' => [
 
-        [ 'href'=> "SceltaDate.php", "text"=>"Prenota", "active" => false ],
-        [ 'href'=> "InserisciCasa.php", "text"=>"Affitta", "active" => false ],
-        [ 'href'=> "#footer", "text"=>"Contattaci", "active" => false ]
-    ]
+
+
+if (isset($_POST["invio"])&&isset($_POST["cerca"])){
+  $cerca = $_POST["cerca"];
+  $data = request($client,$cerca,$api,$index,$data);
+}
+
+for ($i=0; $i<10; $i++){
+  if (isset($_POST[$i])){
+    $index = $_POST[$i];
+  }
+}
+
+if (isset($index)){
+  $index = $index*20;
+  $data = request($client,$cerca,$api,$index,$data);
+}
+
+
+
+
+echo $template->render([
+  'dati' => $data
 ]);
 
 ?>
